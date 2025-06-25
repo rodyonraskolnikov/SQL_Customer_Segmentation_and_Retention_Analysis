c38f3b2a82a0d3d338d17858b9515fd21104f233
rodyonraskolnikov <ozgur.ozturk.professional@gmail.com>
2025-06-25 12:20:37 +0300

    wed_cmmt

--- 1_customer_segmentation.sql
+++ 1_customer_segmentation.sql
@@ -13,7 +13,7 @@ WITH customer_ltv AS (
 		PERCENTILE_CONT(0.25) WITHIN GROUP (ORDER BY total_ltv) AS ltv_25th_percentile,
 		PERCENTILE_CONT(0.75) WITHIN GROUP (ORDER BY total_ltv) AS ltv_75th_percentile
 	FROM customer_ltv
-)
+), segment_values AS (
 SELECT
 	c.*,
 	CASE
@@ -23,3 +23,10 @@ SELECT
 	END AS customer_segment
 FROM customer_ltv c,
 	customer_segments cs
+)
+SELECT
+	customer_segment,
+	SUM(total_ltv) AS total_ltv
+FROM segment_values
+GROUP BY
+	customer_segment 
