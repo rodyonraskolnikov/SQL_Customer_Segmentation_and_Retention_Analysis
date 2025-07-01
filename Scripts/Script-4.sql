436d9d4851f2b70aeba94644f6f2e7aadf245294
rodyonraskolnikov <ozgur.ozturk.professional@gmail.com>
2025-07-01 12:18:21 +0300

    unnecessary_files_remove

---
+++ Scripts/Script-4.sql
@@ -0,0 +1,26 @@
+-- DROP VIEW cohort_analysis;
+
+CREATE OR REPLACE VIEW public.cohort_analysis AS
+WITH customer_revenue AS (
+         SELECT s.customerkey,
+            s.orderdate,
+            sum(s.quantity::double precision * s.netprice * s.exchangerate) AS total_net_revenue,
+            count(s.orderkey) AS num_orders,
+            c.countryfull,
+            c.age,
+            c.givenname,
+            c.surname
+           FROM sales s
+             LEFT JOIN customer c ON c.customerkey = s.customerkey
+          GROUP BY s.customerkey, s.orderdate, c.countryfull, c.age, c.givenname, c.surname
+        )
+ SELECT customerkey,
+    orderdate,
+    total_net_revenue,
+    num_orders,
+    countryfull,
+    age,
+    CONCAT(TRIM(givenname), ' ', TRIM(surname)) AS cleaned_name,
+    min(orderdate) OVER (PARTITION BY customerkey) AS first_purchase_date,
+    EXTRACT(year FROM min(orderdate) OVER (PARTITION BY customerkey)) AS cohort_year
+   FROM customer_revenue cr;
